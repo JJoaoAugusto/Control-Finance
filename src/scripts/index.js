a6cab1bf26c839e4f1bb5004ebac7f568b60acca
João Augusto <jaugustovpires@gmail.com>
2023-04-17 01:42:10 -0300

    controlFinanceV1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,120 @@
-/* Desenvolva sua lógica aqui */
+// REMOVENDO REGISTROS DE VALORES
+function removeRegisters(){
+    const list = document.querySelector('.registers__list')
+    list.innerHTML = ''
+}
+
+// CRIANDO CARDS
+
+function createCard (arrayCategory, element) {
+
+    const list = document.querySelector('.registers__list')
+    const card = document.createElement('li')
+    card.classList.add('list__item')
+    card.id = element.id
+    list.appendChild(card)
+
+    const value = document.createElement('span')
+    value.classList.add('item__value')
+    value.innerText = `R$ ${element.value.toFixed(2)}`
+    card.appendChild(value)
+
+    const asideValue = document.createElement('div')
+    asideValue.classList.add('item__aside')
+    card.appendChild(asideValue)
+
+    const category = document.createElement('span')
+    category.classList.add('item__category')
+    category.id = element.id
+    category.innerText = arrayCategory[element.categoryID]
+    asideValue.appendChild(category)
+
+    const remove = document.createElement('img')
+    remove.src = './src/assets/trash.svg'
+    remove.classList.add('item__remove')
+    asideValue.appendChild(remove)
+
+    const total = document.querySelector('.sum__content')
+    const totalValue = insertedValues.reduce((accumulator, element) => accumulator + element.value, 0);
+    total.innerHTML = `R$ ${totalValue.toFixed(2)}` 
+
+}
+insertedValues.forEach(element => createCard(valuesCategory, element)) 
+
+// FILTRANDO POR TODOS
+
+function filterAll(array) {
+    const main = document.querySelector('.main')
+    main.addEventListener("click", (event) => {
+        if(event.target.classList.contains('todos')){
+            removeRegisters()
+            array.forEach(element => createCard(valuesCategory, element))
+            const totalValue = array.reduce((accumulator, element) => accumulator + element.value, 0);
+            const total = document.querySelector('.sum__content')
+            total.innerHTML = `R$ ${totalValue.toFixed(2)}` 
+        }
+    })
+}
+filterAll(insertedValues)
+
+// FILTRANDO POR ENTRADAS
+
+function filterInflows(array){
+    const main = document.querySelector('.main')
+    main.addEventListener('click', (event) => {
+        if(event.target.classList.contains('entradas')){
+            removeRegisters()
+            let filterIn = array.filter(element => {
+                if(element.categoryID === 0){
+                    return {element}
+                }
+            })
+            filterIn.forEach(element => createCard(valuesCategory, element))
+            const totalValue = filterIn.reduce((accumulator, element) => accumulator + element.value, 0);
+            const total = document.querySelector('.sum__content')
+            total.innerHTML = `R$ ${totalValue.toFixed(2)}`
+        }
+    })
+}
+filterInflows(insertedValues)
+
+// FILTRANDO POR SAÍDAS
+
+function filterOutflows(array){
+    const main = document.querySelector('.main')
+    main.addEventListener('click', (event) => {
+        if(event.target.classList.contains('saidas')){
+            removeRegisters()
+            let filterOut = array.filter(element => {
+                if(element.categoryID === 1){
+                    return {element}
+                }
+            })
+            filterOut.forEach(element => createCard(valuesCategory, element))
+            const totalValue = filterOut.reduce((accumulator, element) => accumulator + element.value, 0);
+            const total = document.querySelector('.sum__content')
+            total.innerHTML = `R$ ${totalValue.toFixed(2)}`
+        }
+    })
+}
+filterOutflows(insertedValues)
+
+// REMOVENDO REGISTRO DE VALOR ESPECÍFICO
+
+function removeValue(array){
+    const main = document.querySelector('.main')
+    main.addEventListener('click', (event) => {
+        if(event.target.classList.contains('item__remove')){
+            const elementId = event.composedPath()[2].id
+            array.forEach((element, indice) => {
+               if(element.id == elementId){
+                return array.splice(indice, 1)
+               }
+            })
+            removeRegisters()
+            array.forEach(element => createCard(valuesCategory, element))
+        }
+    })
+}
+removeValue(insertedValues)
+
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,70 @@
-/* Desenvolva sua lógica aqui */
+// MOSTRANDO MODAL
+
+function handleModal(){
+    const body = document.querySelector('body')
+    body.addEventListener('click', (event) => {
+        if(event.target.classList.contains('register')){
+            const modal = document.querySelector('dialog')
+            modal.showModal()
+        }
+    })
+}
+handleModal()
+
+// FECHANDO MODAL
+
+function closeModal(){
+    const modal = document.querySelector('dialog')
+    modal.addEventListener('click', (event) => {
+       if(event.target.classList.contains('modal__close')){
+        modal.close()
+       }
+    })
+}
+closeModal()
+
+// REGISTRANDO NOVO ELEMENTO
+
+const handleRegisterForm = () => {
+    const modal = document.querySelector('dialog')
+    const inputs = document.querySelectorAll('input')
+    const button = document.querySelector('.modal__submit')
+    let newClient = {}
+    let count = 0
+
+    button.addEventListener('click', (event) => {
+        event.preventDefault()
+        
+        insertedValues.filter(element => {
+            let biggerId = 0
+            if(element.id > biggerId){
+                biggerId = element.id
+                newClient.id = biggerId + 1
+            }
+        })
+
+        inputs.forEach(input => {
+            if(input.value === ''){
+                count++
+            }
+            newClient[input.name] = Number(input.value)
+        })
+
+        if(count !== 0){
+            count = 0
+            return alert('Por favor preencha todos os campos to formulário')
+        }
+
+        insertedValues.push(newClient)
+        newClient = {}
+
+        inputs.forEach(input => {
+            input.value = ''
+        })
+
+        removeRegisters()
+        insertedValues.forEach(element => createCard(valuesCategory, element))
+        modal.close()
+    })
+}
+handleRegisterForm()
